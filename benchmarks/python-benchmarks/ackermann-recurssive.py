8f2d12dbc491a75ffe33f52fa89f0ccedb5f3436
Diablo2050 <amrmrasad@mail.com>
2021-03-29 06:57:08 +0200

    added ackermann csv

--- benchmarks/python-benchmarks/ackermann-recurssive.py
+++ benchmarks/python-benchmarks/ackermann-recurssive.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import csv
 
 def ack1(M, N):
     return (N + 1) if M == 0 else (
@@ -7,12 +8,23 @@ def ack1(M, N):
 
 def test():
     start = datetime.now()
-    print(0, 5)
-    print(f'Result: {ack1(0, 5)}')
+    #print(0, 5)
+    #print(f'Result: {ack1(0, 5)}')
     end = datetime.now()
     delta = end - start
 
-    print(f'{delta.total_seconds():.3f}s')
+    return f'{delta.total_seconds():.3f}s'
 
 
-test()
+test_result = test()
+
+with open('results.csv', mode='w') as csv_file:
+    fieldnames = ['Language', 'Test Name', 'Input', 'Time']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow({'Language': 'python', 'Test Name': 'ackermann-recurssive', 'Input': '(0 & 5)','Time': test_result})
+
+with open('results.csv','r', newline='') as file:
+    reader = csv.reader(file, delimiter=',', quoting=csv.QUOTE_NONE)
+    for row in reader:
+        print(f'{row[0]:<15}  {row[1]:<20} {row[2]:<15} {row[3]:<15}')
